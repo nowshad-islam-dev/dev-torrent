119db52846635c2aef4e4d1657fc8bd25feeb9c4
nowshad-islam-dev <mdnowshadislam.2108033.ruet.mte@gmail.com>
2025-09-10 15:46:40 +0600

    refactor

--- index.js
+++ index.js
@@ -11,12 +11,3 @@ const torrent = bencode.decode(fs.readFileSync('puppy.torrent'), ['utf-8']);
 getPeers(torrent, (peers) => {
   console.log('list of peers', peers);
 });
-
-const socket = dgram.createSocket('udp4');
-const msg = Buffer.from('hello!', 'utf-8');
-
-socket.send(msg, 0, msg.length, my_url.port, my_url.host, () => {});
-
-socket.on('message', (msg) => {
-  console.log('message is:', msg);
-});
